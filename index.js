8b1383fb551d3a34b22d77bdfeed8a6f5dcc8514
Diana <dianabacioccoart@gmail.com>
2021-10-12 21:52:29 -0700

    commit tree 7e74d5620ec457639337fa31ceb57fd45a36ec06

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activityMonday="go to the office") {
+    return `This Monday, I will ${activityMonday}.`;
+}
+function wrapAdjective(adjective="*") {
+        function insideFunction(adjectiveB="special") {
+           return `You are ${adjective}${adjectiveB}${adjective}!`
+        }
+        return insideFunction
+}
+wrapAdjective("%")("a dedicated programmer")
